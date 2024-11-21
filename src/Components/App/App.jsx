dafe1db48e1117eabd21b8b6e55c0840a46c3df7
IvanGudiuk <116677176+IvanGudiuk@users.noreply.github.com>
2024-11-21 20:27:08 +0300

    add

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -12,20 +12,15 @@ import NotFound from "../../Pages/NotFound";
 export const App = () => {
   return (
     <Routes>
-      {/* Standalone routes */}
-      <Route path="/payment/:userId/:screener" element={<Payment />} />
-      <Route path="payment/success" element={<Success />} />
-      <Route path="payment/error" element={<Error />} />
-
-      {/* Nested routes under SharedLayout */}
-      {/* <Route path="/" element={<SharedLayout />}>
+      <Route path="/" element={<SharedLayout />}>
         <Route path="pump" element={<Pump />} />
         <Route path="openinterest" element={<OpenInterest />} />
         <Route path="orderbook" element={<OrderBook />} />
         <Route path="volumes" element={<Volumes />} />
-      </Route> */}
-
-      {/* Catch-all 404 route */}
+      </Route>
+      <Route path="/payment/:userId/:screener" element={<Payment />} />
+      <Route path="/payment/success" element={<Success />} />
+      <Route path="/payment/error" element={<Error />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
