48c77d72a0ba8167b034d55ba7d3ffd6198fa436
IvanGudiuk <116677176+IvanGudiuk@users.noreply.github.com>
2024-11-21 21:22:51 +0300

    add

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -18,9 +18,9 @@ export const App = () => {
         <Route path="openinterest" element={<OpenInterest />} />
         <Route path="orderbook" element={<OrderBook />} />
         <Route path="volumes" element={<Volumes />} />
-        <Route path="/payment/:userId/:screener" element={<Payment />} />
-        <Route path="/payment/success" element={<Success />} />
-        <Route path="/payment/error" element={<Error />} />
+        <Route path="payment/:userId/:screener" element={<Payment />} />
+        <Route path="payment/success" element={<Success />} />
+        <Route path="payment/error" element={<Error />} />
 
         <Route path="*" element={<NotFound />} />
       </Route>
